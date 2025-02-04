64d810d960a967542ddfeaa51b376b157518136b
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-05 00:41:48 +0600

    string boolean

--- milestone_4/first.js
+++ milestone_4/first.js
@@ -1,3 +1,17 @@
 var age = 33;
 console.log(age)
 
+var name = "alif khan";
+var address = "mirpur dhaka 1206";
+var country = "Bangladesh";
+
+//boolean
+var isRich = true;
+var passed = false;
+
+console.log(passed);
+console.log(isRich);
+console.log(name);
+console.log(address);
+console.log(country);
+console.log(typeof country);
