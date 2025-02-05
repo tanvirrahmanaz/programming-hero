91eab46271d7a499cd1cc8ffd929ad7de9676942
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-06 02:18:16 +0600

    ternary opertr

--- milestone_4/comparison.js
+++ milestone_4/comparison.js
@@ -12,3 +12,17 @@ if(weight<20){
 else{
     console.log("I will rent a rickshaw");
 }
+
+weight<20 ? console.log("carry myself") : console.log("rent a rickshaw")
+
+var price = 500;
+var isLeader = true;
+if(isLeader === true){
+    price = 0;
+}
+else{
+    price = price + 100;
+}
+console.log(price)
+
+price = isLeader === true ? 0:price + 100; 
