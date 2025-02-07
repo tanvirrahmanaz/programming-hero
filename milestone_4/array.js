c519384530f2f7a925c9c1c57d6df749bacbb2f4
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-07 12:39:13 +0600

    array in js

---
+++ milestone_4/array.js
@@ -0,0 +1,13 @@
+const number = [1,2,3,4,5,6];
+console.log(number)
+
+number.pop();
+console.log(number)
+
+number.push(8);
+console.log(number)
+
+const out1 = number.pop();
+const out2 = number.pop();
+
+console.log(out1, out2)
