d6685162209ade7dc2e25b9760038ad4d885f7d8
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-07 12:51:07 +0600

    index,includes,pop add

--- milestone_4/array.js
+++ milestone_4/array.js
@@ -7,7 +7,16 @@ console.log(number)
 number.push(8);
 console.log(number)
 
+number.shift();
+console.log(number) //shift array er first digit k remove kore
+
 const out1 = number.pop();
 const out2 = number.pop();
 
 console.log(out1, out2)
+
+console.log(number.includes(3))
+console.log(number.includes(9))
+
+console.log(number.indexOf(4))
+console.log(number.indexOf(1))
