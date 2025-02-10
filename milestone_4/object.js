26918e559c192e81e24ee45d4f4374e2df7b6fe8
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-10 12:16:45 +0600

    object in js

---
+++ milestone_4/object.js
@@ -0,0 +1,15 @@
+const age = 21;
+const school = "rifle square";
+const isPassed = true;
+let isDeveloper;
+const subject = ["bangla","English","Physics","Math"]
+
+
+// object 
+
+const bottle = {
+    brand : "Apple",
+    price : 45,
+    color : "White",
+    isClean : false
+}
