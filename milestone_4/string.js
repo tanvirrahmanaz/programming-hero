06c208140540702317b517ba8f182641082426f4
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-10 11:56:44 +0600

    string

---
+++ milestone_4/string.js
@@ -0,0 +1,24 @@
+const country = 'Bangladesh';
+const division = 'NewKhale';
+const district = `B-baria`;
+const thana = new String("demra");
+
+console.log(typeof thana)
+console.log(thana)
+console.log(division)
+
+console.log(country[3])
+
+// tolowercase,to uppercase, trin(string er samne pisae jody kono space thkae tkhn eita use korte hoi )
+
+const part = division.slice(3,);
+console.log(part)
+
+const sentence = "I am a good and hardworking person.";
+console.log(sentence.split(' '))
+
+const first = 'abid';
+const last = 'Nabid';
+
+const fullname = first + " " + last;
+console.log(fullname)
