0876a8250daa01b567337b21d15be0a1a9158e38
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-21 14:22:10 +0600

    object

---
+++ milestone_5/object.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'sakib khan',
+    id : 121,
+    address: 'movie cinema',
+    friends : ['apu','raaz','salman','amin'],
+    car: {
+        brand: 'tesla',
+        price: 500000,
+        made: 2025
+    }
+}
+
+console.log(student.car)
