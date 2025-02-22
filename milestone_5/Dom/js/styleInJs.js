f78183403bea087c7f6c4d5511bcb1efcc2df749
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-22 13:18:43 +0600

    ..

--- milestone_5/Dom/js/styleInJs.js
+++ milestone_5/Dom/js/styleInJs.js
@@ -1,5 +1,13 @@
-const sections = document.querySelectorAll('section');
-for(const section of sections){
-    section.style.border = '2px solid red'
-    section.style.marginBottom = '5px'
-}
+// const sections = document.querySelectorAll('section');
+// for(const section of sections){
+//     section.style.border = '2px solid red'
+//     section.style.marginBottom = '5px'
+// }
+
+
+
+const placeList = document.getElementById('list3');
+const li = document.createElement('li');
+li.innerText= 'pahartoli bon';
+
+placeList.appendChild(li);
