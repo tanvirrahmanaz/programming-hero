55dde883199f23543f381d8242395451a90d31fd
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-02-23 01:02:30 +0600

    ..

--- milestone_5/Dom2/JS/dom.js
+++ milestone_5/Dom2/JS/dom.js
@@ -1,1 +1,18 @@
 console.log('External file')
+
+// option 2 event hadle 
+function makeYellow(){
+    document.body.style.backgroundColor= 'yellow';
+}
+function makeRed(){
+    document.body.style.backgroundColor= 'red';
+} 
+
+
+// option 3 elements by id 
+const btnMakeBlue= document.getElementById('btn-make-blue')
+btnMakeBlue.onclick = function btnMakeBlue(){
+    document.body.style.backgroundColor= 'blue' 
+}
+
+
