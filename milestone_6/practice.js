d538b1779993bfaef4689ca2b1b3bce8b4e78170
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-03-11 23:41:17 +0600

    falsy value

--- milestone_6/practice.js
+++ milestone_6/practice.js
@@ -1,2 +1,14 @@
-let name = null;
-console.log(typeof name)
+// // // let name = null;
+// // // console.log(typeof name)
+
+// // console.log(names)  --> undefined
+
+// const name = "TANVIR RAHMAN"
+// if(name){
+//     console.log("This is truthly value")
+// }
+// else{s
+//     console.log('this is falsy value')
+// }
+
+// 0 falsy value,none string falsy value
