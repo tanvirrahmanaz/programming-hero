a6d6178cf14b9074735041a3020893a68485bbd2
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-03-13 13:33:15 +0600

    json add and appendchild

--- milestone_6/practice.js
+++ milestone_6/practice.js
@@ -22,5 +22,18 @@
 
 
 const loadUser = () =>{
-    console.log('hello world')
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => showUser(data))
+}
+
+const showUser = (users) => {
+
+    const userContainer = document.getElementById('users');
+
+    for(const user of users){
+        const li = document.createElement('li');
+        li.innerText= user.name;
+        userContainer.appendChild(li)
+    }
 }
