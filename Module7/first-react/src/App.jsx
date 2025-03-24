27519d3b191ffbdb46661bba69da2b2e02414c14
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-03-24 11:33:14 +0600

    react

--- Module7/first-react/src/App.jsx
+++ Module7/first-react/src/App.jsx
@@ -8,28 +8,37 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello vai brother</h1>
+      <Person></Person>
+      <Person></Person>
+      <Developer  name='tanvir' tech = "python"></Developer>
+      <Developer  name='rajib' tech = "java"></Developer>
     </>
   )
 }
+
+function Person(){
+  return(
+    <div className='person'>
+      <h1>i am person</h1>
+      <p>You are a gay</p>
+    </div>
+  )
+}
+
+function Developer(props){
+  
+
+  return(
+    <div style={{
+      border: '2px solid red',
+      borderRadius: '20px'
+    }}>
+      <h3>Developer: {props.name}</h3>
+      <p>Technology: {props.tech}</p>
+    </div>
+  )
+}
+
 
 export default App
