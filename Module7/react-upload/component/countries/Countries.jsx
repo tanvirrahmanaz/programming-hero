1577d02c96324762576f9e19e5a3fe78717ef20e
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-02 13:32:43 +0600

    button handler

--- Module7/react-upload/component/countries/Countries.jsx
+++ Module7/react-upload/component/countries/Countries.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {use} from 'react'
 import Country from '../country/Country';
+import './Countried.css'
 
 
 
 const Countries = ({dataPromise}) => {
+    const [visitedCountries, setVisitedCountires] = useState([])
 
     const countries = use(dataPromise);
     
-
+    const handlevisitedCountires = (country) => {
+        console.log('country visited click to be added',country.name.common);
+    }
     return (
         <div>
            <h1>Travelling Countires {countries.length}</h1> 
-           {
-           countries.map(countryData => 
-            <Country key={countryData.cca3} country={countryData} />
-          )
-           }
+           <h3>Traveled so far: </h3>
+           <div  className='countries'>
+                {
+                countries.map(countryData => 
+                    <Country 
+                    key={countryData.cca3} 
+                    country={countryData} 
+                    handleVisitedCountires={handlevisitedCountires}
+                    />
+                )
+                }
+           </div>
         </div>
     );
 };
--- Module7/react-upload/component/country/Country.jsx
+++ Module7/react-upload/component/country/Country.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country , handleVisitedCountires}) => {
+
+    const [visited, setVisited] = useState(false)
+    //console.log(handleVisitedCountires)
+
+    const handleVisited = () =>{
+       
+    //    if(visited === true){
+    //     setVisited(false)
+    //    }
+    //    else{
+    //     setVisited(true)
+    //    }
+
+        setVisited(!visited)
+        handleVisitedCountires(country)
+    }
+
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'}`}>
             <h2>Name: {country.name.common}</h2>
             <img src={country.flags.png} alt="" />
             <p>Independent: {country.independent ? "Free" : "Not Free"}</p>
+            <button className={visited ? 'btn-visited' : 'btn-not-visited'} onClick={handleVisited}>{
+                visited ? "Visited" : "No visited"
+                }</button>
         </div>
     );
 };
