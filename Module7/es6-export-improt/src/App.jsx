f244f422d5e931a329e9b79a3ceadfcb4ae00ce3
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-03 12:05:57 +0600

    none

--- Module7/es6-export-improt/src/App.jsx
+++ Module7/es6-export-improt/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { add, diff, mult } from '../utilites/math/math'
 
@@ -11,30 +9,18 @@ function App() {
   const subs = diff( 3,2);
   const mul = mult(2,2)
 
-  console.log(sum, subs, mul)
+  // console.log(sum, subs, mul)
+
+  const bottles = [
+    {id:1, name:'Pink Nike bottle' , price:250, color:'pink'},
+    {id:2, name:'Pink Nike bottle' , price:250, color:'pink'},
+    {id:3, name:'Pink Nike bottle' , price:250, color:'pink'},
+    {id:4, name:'Pink Nike bottle' , price:250, color:'pink'},
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Hello World</h1>
     </>
   )
 }
---
+++ Module7/es6-export-improt/utilites/tax/tax.js
@@ -0,0 +1,3 @@
+import { mult } from "../math/math";
+
+const calculateTax = mult(2000, .15);
