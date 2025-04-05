a386e1e424506338f9df1485349218feea2ac3fc
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-06 02:45:31 +0600

    none

--- Module7/my-blog/src/components/Blogs/Blogs.jsx
+++ Module7/my-blog/src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
 
@@ -13,6 +14,11 @@ const Blogs = () => {
     return (
         <div>
             <h1 className='text-2xl '>total: {blogs.length}</h1>
+            <div className="all-blogs grid grid-cols-2 ">
+                {
+                    blogs.map((blog) => <Blog blog = {blog}></Blog>)
+                }
+            </div>
         </div>
     );
 };
