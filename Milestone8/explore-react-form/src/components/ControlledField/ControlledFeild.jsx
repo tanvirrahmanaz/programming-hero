84ff4bb0a01f86ae09e8b381edca29b96a643d5b
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-22 10:38:29 +0600

    form

--- Milestone8/explore-react-form/src/components/ControlledField/ControlledFeild.jsx
+++ Milestone8/explore-react-form/src/components/ControlledField/ControlledFeild.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ControlledFeild = () => {
+    const[password,setpassword] = useState('')
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log("submitted");
+
+    }
+    const handlePassword = e =>{
+        console.log(e.target.value)
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input type="email" name='email' placeholder='Email' required/><br />
+                <input type="password" name='password' placeholder='password' onChange={handlePassword} defaultValue={password} required /><br />
+                <input type="submit" value="submit" />
+            </form>
         </div>
     );
 };
