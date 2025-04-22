65f3c32b184fb15e1c7b224ab9f0a09253b4ff1b
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-22 09:43:44 +0600

    form

---
+++ Milestone8/explore-react-form/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Simpleform from './components/Simpleform/Simpleform'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>
+        explore react form
+      </h1>
+     
+      <Simpleform></Simpleform>
+    </>
+  )
+}
+
+export default App
---
+++ Milestone8/explore-react-form/src/components/Simpleform/Simpleform.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Simpleform = () => {
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log("Form submitted")
+        console.log(event.target)
+        console.log(event.target.name)
+        console.log(event.target.name.value)
+    }
+
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder='your name' name='name'/>
+                <br />
+                <input type="submit" value="Submit" />
+
+            </form>
+        </div>
+    );
+};
+
+export default Simpleform;
