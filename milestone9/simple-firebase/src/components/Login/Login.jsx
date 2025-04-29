358c8a8a2eb4d24d26f4c3ccf2cb472a0016620e
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-04-29 21:29:16 +0600

    none

--- milestone9/simple-firebase/src/components/Login/Login.jsx
+++ milestone9/simple-firebase/src/components/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from '../../firebase/firebase.init';
+
 
 const Login = () => {
 
+    const provider = new GoogleAuthProvider();
+
+
     const handleLogin = () => {
         console.log('login');
-    }
+
+        signInWithPopup(auth,provider)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+        console.log(error);
+    })
+}
 
     return (
         <div>
