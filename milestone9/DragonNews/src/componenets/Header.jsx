137bb4f33b8be32f4351e7ab1e2c608c2d8ddf94
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-03 00:12:19 +0600

    none

--- milestone9/DragonNews/src/componenets/Header.jsx
+++ milestone9/DragonNews/src/componenets/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
-            <img className='w-[350px] ' src={logo} alt="Dragon News" />
+            <img className='w-[40 0px] ' src={logo} alt="Dragon News" />
             <p className='text-accent'>Journalism without Fear or Favour</p>
+            <p className='text-accent'>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
         </div>
     );
 };
