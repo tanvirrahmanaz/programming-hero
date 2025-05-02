49f11df98bdc19c96bff89dfe1fe820762ee5010
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-03 00:23:28 +0600

    none

---
+++ milestone9/DragonNews/src/componenets/LatestNews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LatestNews = () => {
+    return (
+        <div className='bg-base-200 p-5 rounded-lg flex items-center gap-3'>
+        <p className='bg-secondary text-base-100 px-3 py-2'>Latest</p>
+        <p className='font-bold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, modi.</p>
+        </div>
+    );
+};
+
+export default LatestNews;
--- milestone9/DragonNews/src/layouts/HomeLayout.jsx
+++ milestone9/DragonNews/src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../componenets/Header';
+import LatestNews from '../componenets/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto my-3'>
+                    <LatestNews></LatestNews>
+                </section>
             </header>
             <main>
                 <section className='left_nav'></section>
