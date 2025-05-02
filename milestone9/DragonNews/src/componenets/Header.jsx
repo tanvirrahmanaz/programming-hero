8f16c90731f9537a08211f7b5af58fff31de35d3
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-02 23:41:02 +0600

    none

--- milestone9/DragonNews/src/componenets/Header.jsx
+++ milestone9/DragonNews/src/componenets/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='flex justify-center flex-col items-center gap-3'>
+            <img className='w-[350px] ' src={logo} alt="Dragon News" />
+            <p>Journalism without Fear or Favour</p>
         </div>
     );
 };
