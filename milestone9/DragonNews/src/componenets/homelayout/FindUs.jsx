f442e22b68a0d2cde81db80116e39b6d2d0b31d2
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-04 08:24:46 +0600

    none

---
+++ milestone9/DragonNews/src/componenets/homelayout/FindUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FindUs = () => {
+    return (
+        <div>
+            Find us on
+        </div>
+    );
+};
+
+export default FindUs;
--- milestone9/DragonNews/src/componenets/homelayout/RightAside.jsx
+++ milestone9/DragonNews/src/componenets/homelayout/RightAside.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
 
 const RightAside = () => {
     return (
         <div>
             <SocialLogin></SocialLogin>
+            <FindUs></FindUs>
         </div>
     );
 };
