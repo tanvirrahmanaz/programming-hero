4ec3c27c0df588f3c645d5ad587ed4f9e4704b4c
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-13 13:10:35 +0600

    none

--- milestone10/users-managements-client/src/components/Users.jsx
+++ milestone10/users-managements-client/src/components/Users.jsx
@@ -3,9 +3,26 @@ import React,{use} from 'react';
 const Users = ({usersPromise}) => {
     const users = use(usersPromise);
     console.log(users);
-    return (
+
+    const handleAddUser = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const User = {name, email};
+        console.log(User); 
+    }
+    return ( 
         <div>
-            
+
+            <form onSubmit={handleAddUser}> 
+                <input name='name' type="text" /><br />
+                <input name='email' type="email" /><br />
+                <input type="submit" value="Add User" />
+            </form>
+
+            {
+                users.map(user => <p key={user.id}>{user.name} : {user.email}</p>)
+            }
         </div>
     );
 };
--- milestone10/users-managements-server/index.js
+++ milestone10/users-managements-server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-
+app.use(cors()); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
