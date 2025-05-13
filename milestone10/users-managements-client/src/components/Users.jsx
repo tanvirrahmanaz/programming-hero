34afb67c6b0e0e8f2190e9cb59694f2b075bf1a3
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-13 13:43:44 +0600

    none

--- milestone10/users-managements-client/src/components/Users.jsx
+++ milestone10/users-managements-client/src/components/Users.jsx
@@ -10,6 +10,9 @@ const Users = ({usersPromise}) => {
         const email = e.target.email.value;
         const User = {name, email};
         console.log(User); 
+
+        //create user in the server
+        fetch('http://localhost:3000/users').then(res => res.json()).then(data => {console.log(data)});
     }
     return ( 
         <div>
