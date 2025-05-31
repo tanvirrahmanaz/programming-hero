bb2c1f8ff0a98cd99631b980d644b5a2c40fa5c5
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-05-31 11:43:37 +0600

    none

--- milestone11/job-portal/src/main.jsx
+++ milestone11/job-portal/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Router } from 'express'
+import { RouterProvider } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router}>
+      
+    </RouterProvider>
   </StrictMode>,
 )
---
+++ milestone11/job-portal/src/router/router.jsx
@@ -0,0 +1,15 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+
+
+export default router;
