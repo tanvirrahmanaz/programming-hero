bbd794ba92a76bbb1a398326394d6a7742f3e3f2
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-06-05 12:17:38 +0600

    none

--- milestone11/job-portal/src/pages/Home/Banner.jsx
+++ milestone11/job-portal/src/pages/Home/Banner.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { motion } from " motion/react"
+import { motion } from "motion/react"
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <motion.div animate={{rotate:360}} className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
                     className="max-w-sm rounded-lg shadow-2xl"
                 />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Latest Job</h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
@@ -18,7 +18,7 @@ const Banner = () => {
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
