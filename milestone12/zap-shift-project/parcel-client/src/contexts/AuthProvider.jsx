35aa004fbe9f5d98cfcac572690f0033bbc2211b
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-07-05 23:48:56 +0600

    auth provider update

--- milestone12/zap-shift-project/parcel-client/src/contexts/AuthProvider.jsx
+++ milestone12/zap-shift-project/parcel-client/src/contexts/AuthProvider.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init'; // Import the auth instance
 
 const AuthProvider = ({children}) => {
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     const createUser = (email,password) => {
+        setLoading(true);
         // Function to create a new user
         return createUserWithEmailAndPassword(auth, email, password)
     };
 
     const signIn = (email, password) => {
+        setLoading(true);
         // Function to sign in an existing user
         return signInWithEmailAndPassword(auth, email, password);
     };
 
+    useEffect ( () => {
+        const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    })
+
     const authInfo = {
+        user,
+        loading,
         createUser,
         signIn,
 
